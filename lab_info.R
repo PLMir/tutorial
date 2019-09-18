d9821dfeae5efdb85ec5a8cb48a8a7bef68c78c4
PLMir <mirabile.patricia@gmail.com>
2019-09-18 16:26:46 +0000

    locations

--- lab_info.R
+++ lab_info.R
@@ -7,10 +7,12 @@ lab_members <- c("Zach", "Sara", "Grace", "Nicole", "Emily")
 
 lab_pets <- c("Lemon", "Gil")
 
-lab_location <- c("ASU", "Phoenix", "Arizona", "West Campus")
+lab_location <- c("ASU", "Phoenix", "Arizona", "West Campus", "USA", "World")
 
 
 lab_name <- c("CCD Lab")
 
 lab_PIs <- c("Zach", "Angel")
+
+use_git_config(user.name = "PLMir", user.email = "mirabile.patricia@gmail.com")
 
