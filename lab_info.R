4e356e9c36ba5fcc5f9a0ba57f59b6add427a06d
PLMir <mirabile.patricia@gmail.com>
2019-09-18 17:49:47 +0200

    changes

--- lab_info.R
+++ lab_info.R
@@ -2,11 +2,8 @@
 
 
 
-<<<<<<< HEAD
+
 lab_members <- c("Zach", "Sarah", "Grace", "Nicole", "Emily", "modellers")
-=======
-lab_members <- c("Zach", "Sara", "Grace", "Nicole", "Emily", "modelers")
->>>>>>> 7e65db59311e68fd7ed3bc0edc095e765f740499
 
 
 lab_pets <- c("Lemon", "Gil")
